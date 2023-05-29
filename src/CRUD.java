55d71d3148f94a4d15f6f00e5d6aa3a3c2fc17e9
vikas umbadwar <vikasumbadwar@gmail.com>
2023-05-29 09:29:29 +0530

    app.class and java file is deleted and modified crud file

--- src/CRUD.java
+++ src/CRUD.java
@@ -1,9 +1,11 @@
 import java.beans.Statement;
+import java.net.SocketTimeoutException;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class CRUD {
     static String url = "jdbc:mysql://localhost:3306/";
@@ -13,7 +15,28 @@ public class CRUD {
     DB_Connect d = new DB_Connect();
 
     Connection con = d.connect();
+    Scanner sc = new Scanner(System.in);
     
+    System.out.println("Hii, I hope all is well..");
+    System.out.println("What i can help :) please select following choice you want to do..");
+    System.out.println("1.Create Database.");
+    System.out.println("2.Create table.");
+    System.out.println("3.Insert Record.");
+    System.out.println("4.Update Record");
+    System.out.println("5.Delete Record");
+    System.out.println("6.Read the Information.");
+    int Choice = sc.nextInt();
+
+    switch(Choice){
+
+        case 1:
+         
+        break;
+
+        default:
+        System.out.println("please Check your inseting wrong input :)");
+        break;
+    }
     
    }
 
